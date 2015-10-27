04ff80a4e7be23ff62af6122fef615fe08287b0c
Hashma <shahi22h@mtholyoke.edu>
2015-10-27 19:08:16 -0400

    added main

---
+++ cpu.c
@@ -0,0 +1,8 @@
+//parameters: frequency and length
+//PQ: event queue
+//queue: ready queue
+//initialize stats: length of the simulation in time units (specified by user)
+//number of events processed
+//final and average length of the PQ
+//final and average length of the queue
+//For each process: i) number of processes of the same type ii) 
---
+++ main.c
@@ -0,0 +1,5 @@
+//1. name of process
+//2. average CPU time
+//3. average burst time
+//4. average interarrival time
+//5. average time required to se
