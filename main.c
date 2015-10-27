6cfed594ee4e081634772bde8e65e7c3d6ffeacf
Hashma <shahi22h@mtholyoke.edu>
2015-10-27 19:19:45 -0400

    main.c

--- main.c
+++ main.c
@@ -1,5 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 1000
+
 //1. name of process
 //2. average CPU time
 //3. average burst time
 //4. average interarrival time
 //5. average time required to se
+
+int main(int argc, char **argv) {
+    
+    FILE *f = fopen(arg[1], "r")
+    
+    char buff[SIZE];
+    
+    while(fgets(buff, BUFSIZE - 1, fp) != NULL)
+    {
+        /* buff has one line of the file, do with it what you will... */
+        
+        printf ("%s\n", buff); /* ...such as show it on the screen */
+    }
+    fclose(fp); 
+    
+}
