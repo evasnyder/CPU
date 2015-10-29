6db2bbcf18df99a89af93076a86c57cb7fe8bef0
Eva Snyder <snyde24e@mtholyoke.edu>
2015-10-28 20:32:28 -0400

    comments and shit

--- fuck.c
+++ fuck.c
@@ -1,5 +1,16 @@
 
 
 void createEvent(timeStamp, type, process) {
-  
+  // an event needs to be created and added to the event queue
+
+  // set the event type
+
+  // know the average values, use those average values to make own values
+
+  // set its values to the right things
+    // io service, interarrival, burst, cpu service
+}
+
+void removeProcess() {
+
 }
--- fuck.h
+++ fuck.h
@@ -12,14 +12,26 @@ struct Event {
 }
 
 struct Process {
+  // how long I'm stuck in the IO
   int IO_Service;
+
+  // Next time at which a new process will arrive
   int interarrival_time;
+
+  // how long it wants to run before going to IO
   int burst_time;
+
+  // total time on the CPU in the lifetime of the process
   int cpu_service_time;
 
+  // how much burst time there is remaining
   int burst_time_remaining;
+
+  // how much cpu service time there is left for the process
   int cpu_service_time_remaining;
 }
 
 
 void createEvent(timeStamp, type, process);
+
+void removeProcess();
