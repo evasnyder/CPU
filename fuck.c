214db9e26610431f993c9be8ebc7f1e3c3f1e589
Eva Snyder <snyde24e@mtholyoke.edu>
2015-10-28 23:59:10 -0400

    some more shit

--- fuck.c
+++ fuck.c
@@ -1,3 +1,4 @@
+#include "queue.c"
 #include "pqueue.c"
 #include "fuck.h"
 
@@ -16,30 +17,51 @@ void createEvent(timeStamp, type, process) {
   // set the event type
 
   // an event needs to be created and added to the event queue based off of priority
-  enqueue(newEvent);
+  add(newEvent);
 }
 
-void removeProcess(int type) {
+void removeProcess(int type, Event event) {
   switch(type) {
     // terminate
-    // update statistics
     case 2:
+      // free event from memory
 
-    break;
+      // update statistics
+
+      break;
 
     // go to IO
     case 3:
+      // generate a new event to go back to the ready queue
+      struct Event* newEvent = (struct Event*)malloc(sizeof(struct Event));
 
-    break;
+      // initialize new Event...
+      // newEvent -> timeStamp =
+      // newEvent -> type =
+      // set the process of the new event point to the event of the current process
+      newEvent -> process = event -> process;
+
+      // enqueue onto the ready queue
+      enqueue(newEvent);
+      break;
 
     // quantum expire
     case 4:
+      // update information for the event
 
-    break;
+      // go back to the ready queue
+      enqueue(event);
+      break;
   }
 
 }
 
-void generateRandomValues() {
+/**
+* Method to generate the random numbers for the process being created
+* 1 =
+* 2 =
+* 3 = 
+*/
+void generateRandomValues(int processType) {
 
 }
