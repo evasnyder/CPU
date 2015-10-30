b1345a9ed2ccaba6d3bde220b505d720a66bf15c
Hashma <shahi22h@mtholyoke.edu>
2015-10-30 01:37:05 -0400

    bleh

--- switch.c
+++ switch.c
@@ -12,8 +12,10 @@ void print_usage() {
 
 int main(int argc, char *argv[]) {
     int opt= 0;
-    int area = -1, perimeter = -1, breadth = -1;
+    int quantum;
+    int stopTime;
     char *fileName;
+    int numCPU;
     FILE *fp;
 
 
@@ -21,25 +23,40 @@ int main(int argc, char *argv[]) {
     //The two options l and b expect numbers as argument
     static struct option long_options[] = {
         {"fileName",    required_argument, 0,  'f' },
-        {"area",      no_argument,       0,  'a' },
-        {"perimeter", no_argument,       0,  'p' },
-        {"breadth",   required_argument, 0,  'b' },
+        {"numCPU",      required_argument, 0,  'c' },
+        {"quantum", required_argument,       0,  'q' },
+        {"simulationStopTime",   required_argument, 0,  't' },
+        {"switchTime", no_argument,       0,  'q' },
+        {"noIOFaults",   no_argument, 0,  't' },
+        {"verbose", no_argument,       0,  'q' },
+        {"batch",   no_argument, 0,  't' },
+        {"help",   no_argument, 0,  't' },
         {0,           0,                 0,  0   }
     };
 
     int long_index =0;
-    while ((opt = getopt_long(argc, argv,"apf:b:", 
+    while ((opt = getopt_long(argc, argv,"apf:b:c:q:t:w:n:v:b:h:", 
                    long_options, &long_index )) != -1) {
         switch (opt) {
             case 'f' : fileName = optarg;
-              printf("Area: %s\n",optarg); 
+              printf("fileName: %s\n",optarg); 
                  break;
-             case 'a' : area = 0;
+             case 'c' : numCPU = atoi(optarg);
                  break;
-             case 'p' : perimeter = 0;
+             case 'q' : quantum = atoi(optarg);
                  break;
-             
-             case 'b' : breadth = atoi(optarg);
+             case 't' : stopTime = atoi(optarg);
+                 break;
+              case 'w' : //get context switch cost
+                 break;
+              case 'n' : //disable I/O faults
+                 break;
+              case 'v' : //enable verbose output
+                 break;
+              case 'b' : //display parsaeble batch output
+                 break;
+              case 'h' : //display help message
+                  
                  break;
              default: print_usage(); 
                  exit(EXIT_FAILURE);
@@ -48,6 +65,10 @@ int main(int argc, char *argv[]) {
 
 
      printf("Area: %s\n",fileName); 
+     printf("CPU: %d\n",numCPU); 
+     printf("Quantum: %d\n",quantum); 
+     printf("StopTime: %d\n",stopTime); 
+
     fp = fopen(fileName, "r");
      
     char s1[200],*p;
