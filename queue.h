63da7711c67460ab53263682b5db25c9a779ebdb
Hashma <shahi22h@mtholyoke.edu>
2015-10-30 00:28:52 -0400

    queue.c

--- queue.h
+++ queue.h
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
-struct Process {
-	struct Process *next_process;
-	int avg_CPU_Service_Time;
-	int avg_Burst_Time;
-  int avg_Interarrival_Time;
-  int avg_IO_Service_Time;
-};
-
 void enqueue (int value);
 
 void dequeue();
