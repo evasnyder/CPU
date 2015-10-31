3b9a7b40ee2834204a8f5d72d194567990954504
Eva Snyder <snyde24e@mtholyoke.edu>
2015-10-31 10:40:58 -0400

    somewhat of a fix

--- pqueue.h
+++ pqueue.h
@@ -1,13 +1,13 @@
-#ifdef pqueue_h_
+// #ifdef pqueue_h_
 #include <stdio.h>
 
 //function signatures for the main Priority Queue methods
 void add(struct Event* newEvent);
 int sizePQ();
 int getFirst();
-struct Event *deletePQ();
+struct Event* deletePQ();
 //event deletePQ();
 int isEmptyPQ();
 void printPQ();
 
-#endif
+// #endif
